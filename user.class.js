c1e6dcadff0f702cdec8dbfb12cab79a339216e0
Jason Walworth <jrwalworth@gmail.com>
2022-05-24 12:06:42 -0700

    Method Chaining.

--- user.class.js
+++ user.class.js
@@ -6,22 +6,26 @@ class User {
     }
     makeDeposit(amount) {
         this.accountBalance += amount;
+        return this
     }
     makeWithdrawal(amount) {
         if( amount <= this.accountBalance){
             this.accountBalance -= amount;
+            return this
         } else {
             console.log('Insufficient funds!')
         }
         
     }
     displayBalance() {
-        return console.log(this.accountBalance);
+        console.log(this.accountBalance);
+        return this
     }
     transferMoney(amount, user) {
         if (amount <= this.accountBalance) {
             this.accountBalance -= amount;
             user.accountBalance += amount;
+            return this
         } else {
             console.log('Transfer Failed - Insufficient funds');
         }
@@ -32,29 +36,17 @@ const Jason = new User('jrwalworth', 'jrwalworth@gmail.com');
 const Eric = new User('emoney', 'e@yahoo.com');
 const Sasha = new User('Sasha1', 'sash@hotmail.com');
 
-console.log('Jason', Jason);
-Jason.makeDeposit(500);
-Jason.makeDeposit(50);
-Jason.makeDeposit(25);
-Jason.makeWithdrawal(5);
-Jason.displayBalance();
-console.log('Jason', Jason);
+console.log('Jason Initial:', Jason);
+Jason.makeDeposit(500).makeDeposit(50).makeDeposit(25).makeWithdrawal(5).displayBalance();
+console.log('Jason After Activity:', Jason);
 
-console.log('Eric', Eric);
-Eric.makeDeposit(2500);
-Eric.makeDeposit(1000);
-Eric.makeWithdrawal(200);
-Eric.makeWithdrawal(300);
-Eric.displayBalance();
-console.log('Eric', Eric);
+console.log('Eric Initial:', Eric);
+Eric.makeDeposit(2500).makeDeposit(1000).makeWithdrawal(200).makeWithdrawal(300).displayBalance();
+console.log('Eric After Activity:', Eric);
 
-console.log('Sasha', Sasha);
-Sasha.makeDeposit(1000);
-Sasha.makeWithdrawal(20);
-Sasha.makeWithdrawal(20);
-Sasha.makeWithdrawal(20);
-Sasha.displayBalance();
-console.log('Sasha', Sasha);
+console.log('Sasha Initial:', Sasha);
+Sasha.makeDeposit(1000).makeWithdrawal(20).makeWithdrawal(20).makeWithdrawal(20).displayBalance();
+console.log('Sasha After Activity', Sasha);
 
 console.log('after $ transfer...');
 Eric.transferMoney(250, Sasha);
